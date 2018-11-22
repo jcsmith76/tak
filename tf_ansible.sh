6dc005e7be84318466fb075c49cdb420a872a915
JC Smith <js@intrusoft.com>
2018-11-21 20:43:13 -0800

    some cleanup

--- tf_ansible.sh
+++ tf_ansible.sh
@@ -1,6 +1,5 @@
-# create inventory from AWS
-echo "" > tf_inventory.txt
-echo "[master]" >> tf_inventory.txt
+# create Ansible inventory from AWS
+echo "[master]" > tf_inventory.txt
 aws ec2 describe-addresses --filter Name=tag:type,Values=tfk8s --output text |grep ADDRESSES|awk '{print $9}' > /tmp/inv.txt
 head -n 1 /tmp/inv.txt >> tf_inventory.txt
 echo "[worker]" >> tf_inventory.txt
@@ -9,12 +8,17 @@ tail -n 2 /tmp/inv.txt >> tf_inventory.txt
 # run playbooks
 ANSIBLE_HOST_KEY_CHECKING=False
 export ANSIBLE_HOST_KEY_CHECKING
+
+# setup apt and python ahead of time if needed
 ansible-playbook -i tf_inventory.txt provision.yml
 sleep 100
+
+# verify all the nodes are up and responding
 ansible all --inventory-file=tf_inventory.txt -m ping -u ubuntu
 sleep 100
-ansible-playbook -i tf_inventory.txt k8s.yml
 
+# install the k8s master, join the workers to the master
+ansible-playbook -i tf_inventory.txt k8s.yml
 
 # After this, you should be able to ssh into master and run
 # kubectl get nodes
